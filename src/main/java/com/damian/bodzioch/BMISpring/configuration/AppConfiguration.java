119f2ace82a141f7ac0ea8c8a8d5116b4e037f4c
OpusMagnus5 <damianbodzioch95@gmail.com>
2022-06-24 12:48:56 +0200

    update

---
+++ src/main/java/com/damian/bodzioch/BMISpring/configuration/AppConfiguration.java
@@ -0,0 +1,9 @@
+package com.damian.bodzioch.BMISpring.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.damian.bodzioch.BMISpring")
+public class AppConfiguration {
+}
---
+++ src/main/java/com/damian/bodzioch/BMISpring/controllers/WebController.java
@@ -0,0 +1,8 @@
+package com.damian.bodzioch.BMISpring.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebController {
+    //TODO 2:28
+}
